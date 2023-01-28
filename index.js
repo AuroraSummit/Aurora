5a96aa72745e4f691e7047975cd20772980ebfe5
AuroraSummit <109423192+AuroraSummit@users.noreply.github.com>
2023-01-28 19:26:36 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||祝||你||生||日||快||乐||#rectangle|");
+      S.UI.simulate(
+        "|||#countdown 3|||祝|||你|||生|||日|||快|||乐|||#rectangle|"
+      );
     }
 
     S.Drawing.loop(function () {
